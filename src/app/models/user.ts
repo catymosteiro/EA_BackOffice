2c817718aa563ac598647a094599a7e4e376b006
Pau Baguer <pauinki@gmail.com>
2022-03-25 16:10:35 +0100

    added chat

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,19 @@
-export class User {
-    _id?: number;
-    id: string;
-    name: string;
-    age: string;
-    password: string;
-
-    constructor(id: string, name: string, age: string, password: string) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.password = password;
-    }   
+import Dates from './dates';
+import { Book } from './book';
+import { Chat } from './chat';
+import { Club } from './club';
+export interface User extends Dates {
+  _id?: string;
+  name: string;
+  userName: string;
+  birthDate: Date;
+  mail: string;
+  password: string;
+  location: { latidude: Number; longitude: Number };
+  books: Book[];
+  events: Event[];
+  clubs: Club[];
+  chats: Chat[];
+  disabled: Boolean;
+  categories: string[];
 }
